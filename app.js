d29fe5a42b0b4eaf1c208dafcca05a3d7b15e2d9
eunwoo <eunwoo123@gmail.com>
2022-02-08 17:43:57 +0900

    CORS 추가

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const livereload = require('livereload')
+const livereloadMiddleware = require('connect-livereload')
+const fs = require('fs')
+const fsPromise = fs.promises;
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const dir = 'D:/prog/kiosk/kiosk/src/static/Home'
+const whitelist = ["http://localhost:3001"]
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not Allowed Origin!"));
+      }
+    },
+};
+const liveServer = livereload.createServer({
+    exts: ['html', 'css', 'js'],
+    debug: true
+})
+liveServer.watch(__dirname);
+const app = express()
+const port = 3001
+app.use(livereloadMiddleware())
+// app.use(cors(corsOptions))
+app.use(cors());
+app.get('/', async (req, res) => {
+    let names;
+    names = await fsPromise.readdir(dir);
+    console.log(names);
+    res.send(JSON.stringify(names));
 })
 
 app.listen(port, () => {
